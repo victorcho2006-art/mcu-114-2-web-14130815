5bb8c7dd0280f7ba720295cfee14ce5d00a42134
卓定騰 <victorcho2006@gmail.com>
2026-03-25 19:04:00 +0800

    feat(ts): 新增與刪除產品資料

--- src/index.ts
+++ src/index.ts
@@ -9,4 +9,30 @@ let products = [
   new Product(6, "書籍 D", "博碩文化", 500, false, new Date("2024-4-10")),
 ];
 
-products.filter((item) => item.isShow).forEach((item) => console.table(item));
+// 新增產品
+const newProduct = new Product(0, "書籍 E", "博碩文化", 500, false, new Date("2024-4-10"));
+addProduct(newProduct);
+console.table(products);
+
+// 刪除產品 By index
+removeProduct(6);
+console.table(products);
+
+removeProductById(4);
+console.table(products);
+
+function addProduct(product: Product): void {
+  const id = products.length === 0 ? 1 : Math.max(...products.map(({ id }) => id)) + 1;
+  product.id = id;
+  products.push(product);
+}
+
+function removeProduct(index: number): void {
+  products.splice(index, 1);
+}
+
+// 刪除產品 By id
+function removeProductById(removeID: number): void {
+  const index = products.findIndex(({ id }) => id === removeID);
+  products.splice(index, 1);
+}
